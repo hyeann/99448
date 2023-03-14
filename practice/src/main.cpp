a464a18a5b322d021155db6f97af004bc58c0e3c
hyeann <127829083+hyeann@users.noreply.github.com>
2023-03-14 16:21:24 +0900

    initialize

---
+++ practice/src/main.cpp
@@ -0,0 +1,22 @@
+
+#include "vex.h"
+
+using namespace vex;
+
+controller ctrl; 
+motor f_right(PORT10,true);
+motor b_right(PORT20,true);
+motor f_left(PORT3);
+motor b_left(PORT12);
+motor_group c_left(f_left,b_left);
+motor_group c_right(f_right,b_right);
+
+
+int main() {
+
+while(true){  
+  c_left.spin(directionType::fwd,ctrl.Axis3.value(),velocityUnits::pct);
+  c_right.spin(directionType::fwd,ctrl.Axis2.value(),velocityUnits::pct);
+  task::sleep(20);
+}
+}
